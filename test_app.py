b0f2d02c5f15dfaa95738ca76282bfd8d5c49aae
AmirhosseinDotZip <101447196+AmirhosseinDotZip@users.noreply.github.com>
2025-10-24 15:08:45 +0330

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import add
+
+def test_add_function():
+    print("Running test...")
+    assert add(2, 3) == 5
+    print("Test passed!")
+
+# This makes the script run the test function
+if __name__ == "__main__":
+    test_add_function()
